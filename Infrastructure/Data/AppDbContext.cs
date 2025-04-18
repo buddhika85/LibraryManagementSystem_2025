a7b9dd39c9d440dc694f6500c9fe81cf595e0cc8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-04-18 12:46:46 +1000

    app DB context added

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        // SQL server connectin string comes in using options parameter
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
