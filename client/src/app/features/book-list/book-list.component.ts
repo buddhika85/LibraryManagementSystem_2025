0530d9ce28455bcfc048db516b41e1b2e1b077bb
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-04-21 14:29:08 +1000

    Added mat-table to display book list

--- client/src/app/features/book-list/book-list.component.ts
+++ client/src/app/features/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
@@ -6,6 +6,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { BookService } from '../../core/services/book.service';
 import { BookWithAuthorListDto } from '../../shared/models/book-with-author-list-dto';
+import { BookWithAuthorsDto } from '../../shared/models/book-with-authors-dto';
 
 @Component({
   selector: 'app-book-list',
@@ -14,17 +15,18 @@ import { BookWithAuthorListDto } from '../../shared/models/book-with-author-list
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+  // Mat-table 
+  displayedColumns: string[] = ['bookId', 'bookTitle', 'bookGenreStr', 'bookPublishedDateStr'];
+  dataSource!: MatTableDataSource<BookWithAuthorsDto>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
 
+  // for data fetching
   private bookService = inject(BookService);
-
   booksWithAuthorList: BookWithAuthorListDto = {
     bookWithAuthorList: [], 
     count: 0
@@ -35,6 +37,10 @@ export class BookListComponent {
     this.bookService.getAllBooksWithAuthors().subscribe({
       next: data => {
         this.booksWithAuthorList = data;
+        // Assign the data to the data source for the table to render
+        this.dataSource = new MatTableDataSource(this.booksWithAuthorList.bookWithAuthorList);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
         console.log(this.booksWithAuthorList);
       },
       error: error => console.error('There was an error!', error),
@@ -42,20 +48,7 @@ export class BookListComponent {
     });
   }
 
-
-  constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
+  // seach filter on top of the table
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -64,62 +57,4 @@ export class BookListComponent {
       this.dataSource.paginator.firstPage();
     }
   }
-
 }
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
-}
-
-
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
