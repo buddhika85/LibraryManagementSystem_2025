2b1d473ce74def3781a7433dfa6e2ca943786e8e
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-04-25 06:45:01 +1000

    environment.ts added for settings

--- client/src/app/core/services/book.service.ts
+++ client/src/app/core/services/book.service.ts
@@ -3,16 +3,23 @@ import { inject, Injectable } from '@angular/core';
 import { BookWithAuthorListDto } from '../../shared/models/book-with-author-list-dto';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiBaseUrl; //'https://localhost:5001/api/';
   private http = inject(HttpClient);
   
   getAllBooksWithAuthors() : Observable<BookWithAuthorListDto> 
   {
     return this.http.get<BookWithAuthorListDto>(this.baseUrl + 'books');
+  }
+
+  getBookForEditOrInsert(id: number) : Observable<BookWithAuthorListDto>
+  {
+    return this.http.get<BookWithAuthorListDto>(this.baseUrl + 'books/' + id);
   }
 }
