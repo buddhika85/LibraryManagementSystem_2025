21e8fa318881f3bfb7e9ff612d7e3766212ed692
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-04-26 05:00:07 +1000

    other account controller endpoints

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using Core.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -31,5 +34,38 @@ namespace API.Controllers
 
             return Ok();
         }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return NoContent();
+        }
+
+        //[Authorize]
+        [HttpGet("userinfo")]
+        public async Task<ActionResult> GetUserInfo()
+        {
+            if (User.Identity?.IsAuthenticated == false)
+                return NoContent();
+
+            var user = await signInManager.UserManager.Users.FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+            if (user == null)
+                return Unauthorized();
+
+            return Ok(new { 
+                user.FirstName,
+                user.LastName,
+                user.Email
+            });
+        }
+
+        [HttpGet]
+        public ActionResult GetAuthState()
+        {
+            return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated ?? false});
+        }
+
     }
 }
--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,7 @@
 ﻿using Core.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -33,6 +35,15 @@ namespace API.Controllers
         public IActionResult GetValidationError(BookSaveDto item)
         {
             return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            var name = User.FindFirst(ClaimTypes.Name)?.Value;
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Ok($"Hello {name} with id of {id}");
         }
     }
 }
