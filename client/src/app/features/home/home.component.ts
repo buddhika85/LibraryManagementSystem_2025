2b25e06c5d606748b8a24c2ae4f4a1e6599c00be
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-05-02 20:00:33 +1000

    fix in logging page

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../../core/services/account.service';
 import { IsAdminDirective } from '../../shared/directives/is-admin.directive';
 import { IsMemberDirective } from '../../shared/directives/is-member.directive';
 import { IsStaffDirective } from '../../shared/directives/is-staff.directive';
+import { IsLoggedInDirective } from '../../shared/directives/is-logged-in.directive';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,8 @@ import { IsStaffDirective } from '../../shared/directives/is-staff.directive';
      IsAdminDirective,
         IsAdminDirective,
         IsMemberDirective,
-        IsStaffDirective
+        IsStaffDirective,
+        IsLoggedInDirective
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
